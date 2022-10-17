7f34fb618ce774f83bb012933858e8894f7ab311
min33sky <min33sky@naver.com>
2022-10-18 06:47:08 +0900

    feat: add router progress bar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,17 @@ import { SessionProvider } from 'next-auth/react';
 import { Session } from 'next-auth';
 import DialogProvider from '@/contexts/DialogContext';
 
+import Router from 'next/router';
+import NProgress from 'nprogress';
+// import 'nprogress/nprogress.css'; //styles of nprogress
+
+NProgress.configure({ showSpinner: false });
+
+//Route Events.
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
+
 function MyApp({
   Component,
   pageProps: { session, ...pageProps },
